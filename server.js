e60ea6d7f22dce89fee35ed2ae860637a005a33c
Sridhar Vellingiri <sri.rahdirs@gmail.com>
2025-09-05 21:38:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cors({
         'https://staging.nypunyaaesthetics.com',
         'https://dashboard.nypunyaaesthetics.com',
         'https://young-zen-admin-backend.vercel.app',
+        'https://young-zen-admin-frontend.vercel.app/',
     ],
     credentials: true, // important to allow cookies
 }));
